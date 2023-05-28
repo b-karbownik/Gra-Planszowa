c32168391938849edfd23468b5e026db79d6aaf9
KacperJopek <kacperjopek15@gmail.com>
2023-05-28 16:57:28 +0200

    respawn

--- Assets/Scripts/New/Player.cs
+++ Assets/Scripts/New/Player.cs
@@ -9,11 +9,11 @@ public class Player : MonoBehaviour
     public Route currentRoute;
     public int health = 100;
 
-    //inventory-start
     public int fireballAmount;
     public int medkitAmount;
-    
-    //inventory-end
+
+    public int points;
+
     void Start()
     {
         routePosition = 0;
