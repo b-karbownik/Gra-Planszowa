1fc7ba2ae572c18d3b521980e4c0a80897e2cd66
b-karbownik <127301057+b-karbownik@users.noreply.github.com>
2023-05-28 16:22:21 +0200

    Płynny HealthBar

--- Assets/Scripts/New/HealthBar.cs
+++ Assets/Scripts/New/HealthBar.cs
@@ -2,24 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class HealthBar : MonoBehaviour
 {
     public Player player;
     public float playerHealth;
     public Image HealthImage;
+
+    private float targetFillAmount;
+    public float fillSpeed = 5.0f;
+
+    private Coroutine fillCoroutine;
+
     void Start()
     {
-        
+        targetFillAmount = (float)player.health / 100f;
+        HealthImage.fillAmount = targetFillAmount;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            player.health -= 10;
-        }
         playerHealth = player.health;
-        HealthImage.fillAmount = (float)player.health / 100f;
+        float newTargetFillAmount = (float)player.health / 100f;
+        if (newTargetFillAmount != targetFillAmount)
+        {
+            targetFillAmount = newTargetFillAmount;
+
+            if (fillCoroutine != null)
+            {
+                StopCoroutine(fillCoroutine);
+            }
+
+            fillCoroutine = StartCoroutine(FillHealthBar());
+        }
+    }
+
+    IEnumerator FillHealthBar()
+    {
+        float currentFillAmount = HealthImage.fillAmount;
+
+        while (currentFillAmount != targetFillAmount)
+        {
+            currentFillAmount = Mathf.MoveTowards(currentFillAmount, targetFillAmount, fillSpeed * Time.deltaTime);
+            HealthImage.fillAmount = currentFillAmount;
+            yield return null;
+        }
     }
 }
